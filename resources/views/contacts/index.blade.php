c295934c4212cd87f20d002cd1f9e36ab250089d
seyhaktan <seyhak@seyhaktans-MacBook-Air.local>
2023-06-30 22:17:10 +0700

    Hello

---
+++ resources/views/contacts/index.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.main')
+@section('content')
+    <main class="py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header card-title m-0">
+                            <div class="d-flex justify-content-between">
+                                <h4 class="m-0">All Contacts</h4>
+                                <div class="m-0">
+                                    <a href="{{route('contacts.create')}}" class="btn btn-success">
+                                        <i class="bi bi-plus-square"></i> Add New
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            @includeif('contacts._filter')
+                            <div class="table-responsive">
+                                <table class="table table-striped table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">#</th>
+                                            <th scope="col">First Name</th>
+                                            <th scope="col">Last Name</th>
+                                            <th scope="col">Email</th>
+                                            <th scope="col">Company</th>
+                                            <th scope="col">Actions</th>
+                                        </tr>
+                    
+                                    </thead>
+                                    <tbody>
+                                        @forelse($contacts as $key => $contact)
+                                            @includeif('contacts._contact')
+                                        @empty
+                                            @includeif('contacts._empty')
+                                        @endforelse
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                        <nav class="my-2">
+                            <ul class="pagination justify-content-center">
+                                <li class="page-item disabled">
+                                <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a></li>
+                                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>                  
+    </main>
+@endsection
